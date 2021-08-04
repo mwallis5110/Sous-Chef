937364a905a4dc9fb5358178e26c7e751d97585f
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-08-04 12:45:36 -0500

    recipe model, route, seed

---
+++ models/Recipe.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    ingredients: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    directions: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    // user_id: {
+    //     type: DataTypes.STRING,
+    //     references: {
+    //         model: 'user',
+    //         key: 'id'
+    //     }
+    // },
+
+
+
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'recipe',
+});
+
+module.exports = Recipe;

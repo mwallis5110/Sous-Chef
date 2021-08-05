dc5f9ad6ef82d63e39124327ea029454d5b6962c
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-08-04 19:27:19 -0500

    post route, username added, model changed

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ const User = require('./User');
 const Recipe = require('./Recipe');
 
 
-// User.hasMany(Recipe, {
-//     foreignKey: 'user_id',
-// });
+User.hasMany(Recipe, {
+    foreignKey: 'user_id',
+});
 
-// Recipe.belongsToMany(User, {
-//     foreignKey: 'user_id',
-// });
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 module.exports = { User, Recipe };

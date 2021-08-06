af67f1eef85fec6b3762639c683334ee3f2930e4
justinreese160 <84814933+justinreese160@users.noreply.github.com>
2021-08-06 11:42:22 -0500

    clean files

--- controllers/api/recipe.js
+++ controllers/api/recipe.js
@@ -1,51 +1,13 @@
 const router = require('express').Router();
 const { User, Recipe, Recipe_db } = require('../../models');
 router.get("/find-recipes/", async(req, res) => {
-    const recipeData = await Recipe.findAll().catch((err) => {
-        res.json(err);
-    });
-    // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it. 
-    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-    res.status(200).json(recipes)
-})
-//Moved to savedRecipe
-// router.post('/save-recipe', async(req, res) => {
-//         const user_id = req.session.user_id
-//         req.body.user_id = user_id
-//         const response = await Recipe.create(req.body)
-//         try {
-//             // const user_id = req.session.user_id
-//             // req.body.user_id = user_id
-//             const response = await Recipe.create(req.body)
-//             res.status(200).json(response.get({ plain: true }))
-//         } catch (error) {
-//             res.status(500).json(error)
-//         }
-//     })
-    // router.get('/mysavedRecipes', (req, res) => {
-    //     Recipe.findAll(where: {
-    //         user_id: req.body.user_id
-    //     })
-    // })
+        const recipeData = await Recipe.findAll().catch((err) => {
+            res.json(err);
+        });
+        // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it. 
+        const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+        res.status(200).json(recipes)
+    })
+    //Moved to savedRecipe
+
 module.exports = router
-    // GET all blogs for homepage
-    // router.get('/', async(req, res) => {
-    //     try {
-    //         const dbBlogData = await Blog.findAll({
-    //             include: [{
-    //                 model: Comment,
-    //                 attributes: ['filename', 'description'],
-    //             }, ],
-    //         });
-    //         const blogs = dbBlogData.map((blog) =>
-    //             blog.get({ plain: true })
-    //         );
-    //         res.render('homepage', {
-    //             blogs,
-    //             loggedIn: req.session.loggedIn,
-    //         });
-    //     } catch (err) {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     }
-    // });
--- models/Recipe.js
+++ models/Recipe.js
@@ -26,15 +26,7 @@ Recipe.init({
         type: DataTypes.STRING,
         allowNull: false,
     },
-    //Recipe does not need user id
-    // user_id: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull: true,
-    //     references: {
-    //         model: "user",
-    //         key: "id"
-    //     }
-    //}
+
 
 }, {
     sequelize,
@@ -44,27 +36,3 @@ Recipe.init({
 });
 
 module.exports = Recipe;
-
-// Recipe.init({
-//     id: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//         primaryKey: true,
-//         autoIncrement: true,
-//     },
-//     title: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     ingredients: {
-//         type: DataTypes.TEXT,
-//         allowNull: false,
-//     },
-//     directions: {
-//         type: DataTypes.TEXT,
-//         allowNull: false,
-//     },
-//     filename: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//       },

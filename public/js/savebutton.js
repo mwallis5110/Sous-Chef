cd13bc40a1fd02aaca724fb6de3a84a53272639a
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-08-05 20:02:10 -0500

    fixed saved button and created user account with saved recipes

--- public/js/savebutton.js
+++ public/js/savebutton.js
@@ -3,23 +3,27 @@
 //   console.log(e.target.dataset);
 //   console.log(e.target.title);
 // }));
+console.log("JS FILE LAODED!!!!!!!!!!!");
 document.querySelectorAll(".saveButton").forEach((item) => {
     item.addEventListener("click", (event) => {
+        console.log("WE CLICKED THE BUTTON!");
         let data = event.target.dataset;
+        console.log('Data we r gonna save', data)
         let recipe = {
             title: data.title,
-            filename: data.filename,
+            filename: data.image,
             directions: data.directions,
             ingredients: data.ingredients,
             id: data.id,
-        }
+        };
+        console.log("About to save this thing", recipe);
         fetch("/api/savedrecipes", {
             method: "POST",
             headers: {
-                "Accept": "application/json",
+                Accept: "application/json",
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(recipe)
-        })
+            body: JSON.stringify(recipe),
+        });
     });
 });
